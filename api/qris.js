24595b8b0ea1fbebbe58635f4de36fa4a049d521
Dikaajak <andika62696@gmail.com>
2026-04-24 19:26:07 +0700

    Create qris.js

---
+++ api/qris.js
@@ -0,0 +1,29 @@
+export default async function handler(req, res) {
+    if (req.method !== 'POST') {
+        return res.status(405).json({ message: 'Method not allowed' });
+    }
+
+    const { nominal } = req.body;
+    const ATLANTIC_API = "5kijekvBS7FSXIgT6IDNkLm4vJWYbGoqJVNg5ERMZ0HIYwEYx2BZBANcWZ3VKzfyeASSVWEGYiPdOGrZqZvrWjqjIApnmghZRLeY";
+    const nominalFix = parseInt(nominal) + 250;
+    const reffId = "NEXA-" + Date.now();
+
+    try {
+        const response = await fetch("https://atlantich2h.com/deposit/create", {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: new URLSearchParams({
+                api_key: ATLANTIC_API,
+                reff_id: reffId,
+                nominal: nominalFix,
+                type: 'ewallet',
+                method: 'qris'
+            })
+        });
+
+        const data = await response.json();
+        return res.status(200).json(data);
+    } catch (error) {
+        return res.status(500).json({ status: false, message: error.message });
+    }
+}
